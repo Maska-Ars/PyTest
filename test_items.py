8ea9e512247c09539627fdce621483794f78478d
Maska <senu.budnikov@gmail.com>
2025-11-17 08:36:46 +0500

    add files

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_add_to_cart_button_presence(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    time.sleep(30)
+
+    add_to_cart_button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+
+    assert len(add_to_cart_button) > 0, "Add to cart button is not present on the page"
+    assert add_to_cart_button[0].is_displayed(), "Add to cart button is not visible"
+    assert add_to_cart_button[0].is_enabled(), "Add to cart button is not enabled"
